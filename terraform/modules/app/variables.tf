259ef629a9551419d7c3e42e61433e57d434b5ad
Ivan <ivkosarev@gmail.com>
2021-04-11 13:41:00 +0000

    DZtrr2 made, lets try autotests!

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,13 @@
+variable app_disk_image {
+description = "Disk image for reddit app"
+default = "fd8e20auvmrk3cqmkuk2"
+}
+variable service_account_key_file {
+  description = "key.json"
+}
+variable subnet_id {
+description = "Subnets for modules"
+}
+variable public_key_path {
+  default = "~/.ssh/usr1.pub"
+}
---
+++ terraform/modules/db/variables.tf
@@ -0,0 +1,13 @@
+variable db_disk_image {
+description = "Disk image for reddit db"
+default = "fd8r4hajnhrrdq723cqe"
+}
+variable service_account_key_file {
+  description = "key.json"
+}
+variable subnet_id {
+description = "Subnets for modules"
+}
+variable public_key_path {
+  default = "~/.ssh/usr1.pub"
+}
